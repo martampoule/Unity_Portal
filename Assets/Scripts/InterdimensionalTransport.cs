9a650bebdbadfb48b9d75a591aa0b72bb230eef6
marta <marta.cattaneo.96@gmail.com>
2018-11-29 17:10:22 +0100

    portale creato

---
+++ Assets/Scripts/InterdimensionalTransport.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class InterdimensionalTransport : MonoBehaviour 
+{
+
+	public Material[] materials;
+
+	void Start () 
+	{
+		foreach (var mat in materials) 
+		{
+			mat.SetInt ("_StencilTest",(int)CompareFunction.Equal);
+		}
+
+	}
+	
+	void OnTriggerStay(Collider other)
+	{
+		if (other.name != "Main Camera")
+			return;
+
+		//Outside of the other world
+		if (transform.position.z > other.transform.position.z) 
+		{
+
+			Debug.Log ("Outside of the other world");
+			foreach (var mat in materials) 
+			{
+				mat.SetInt ("_StencilTest",(int)CompareFunction.Equal);
+			}
+
+		//Inside other world
+		} else 
+		{
+			Debug.Log ("Inside other world");
+			foreach (var mat in materials) 
+			{
+				mat.SetInt ("_StencilTest", (int)CompareFunction.NotEqual);
+			}
+		}
+	}
+
+	void OnDestroy()
+	{
+		foreach (var mat in materials) 
+		{
+			mat.SetInt ("_StencilTest", (int)CompareFunction.NotEqual);
+		}
+	}
+
+
+	void Update () 
+	{
+		
+	}
+}
---
+++ Assets/Shaders/StencilFilter.shader
@@ -0,0 +1,28 @@
+﻿/*
+ *Most basic shader for only rendereing objects depending on the stencil buffer
+ *And the state of the global _StencilTest property
+ */
+
+
+Shader "Custom/StencilFilter"
+{
+	Properties
+	{
+		_Color ("Color", Color) = (1,1,1,1)
+		[Enum(Equal,3,NotEqual,6)] _StencilTest ("Stencil Test", int) = 6
+
+	}
+	SubShader
+	{	
+
+		Color [_Color]
+		Stencil{
+			Ref 1
+			Comp [_StencilTest]
+		}
+
+		Pass
+		{
+		}
+	}
+}
